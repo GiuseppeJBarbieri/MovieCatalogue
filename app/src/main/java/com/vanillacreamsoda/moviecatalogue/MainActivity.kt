aad310e03a459316bea6812af1588a9fb0b1ae04
Giuseppe Barbieri <Giuseppejbarbieri@gmail.com>
2025-06-10 19:17:24 -0400

    Project Standup
    Standing up movie catalogue app.

---
+++ app/src/main/java/com/vanillacreamsoda/moviecatalogue/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.vanillacreamsoda.moviecatalogue
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.vanillacreamsoda.moviecatalogue.ui.theme.MovieCatalogueTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MovieCatalogueTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MovieCatalogueTheme {
+        Greeting("Android")
+    }
+}
